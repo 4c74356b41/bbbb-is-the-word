cade7a988ec916cabcb98cfb48ab45dfd181d5c4
4c74356b41 <spam@4c74356b41.com>
2016-12-01 16:06:22 +0300

    Update jenkinsjob.ps1

--- jenkinsjob.ps1
+++ jenkinsjob.ps1
@@ -1,27 +1,27 @@
-# H/5 * * * *
+$temp = 0
+$branches = @()
+
+c:\jenkins\git\bin\git.exe branch -a | % { if ($_ -match '\borigin\b' -and $_ -match '\d+'
+) { $branches += $matches[0]} }
+$branches | % { $temp = [math]::Max($_,$temp)}
+$feature = 'feature' + $temp
 
 while ($rgName -notmatch '^[a-z]') {
     $rgName = -join ((97..122) + (48..57) | Get-Random -Count 10 | % {[char]$_})
-    $vaultName = $rgName + '-v'
 }
 $location = @('northeurope','westeurope','eastus2') | get-random
-
-$pwd = (ConvertTo-SecureString -String $env:pwd -AsPlainText -Force)
-$cred = ([pscredential]::new('py@core4c74356b41.onmicrosoft.com',$pwd))
+$cred = ([pscredential]::new('py@core4c74356b41.onmicrosoft.com',(ConvertTo-SecureString -String $env:pwd -AsPlainText -Force)))
 $null = Login-AzureRMAccount -Credential $cred -SubscriptionName 'MCT Subscription'
 $null = New-AzureRmResourceGroup –Name $rgName –Location $location
-$null = New-AzureRmKeyVault -VaultName $vaultName -ResourceGroupName $rgName -Location $location
-$null = Set-AzureRmKeyVaultAccessPolicy -EnabledForTemplateDeployment -VaultName $vaultName
-$null = Set-AzureKeyVaultSecret -VaultName $vaultName -Name 'administratorLoginPassword' -SecretValue $pwd
 
 $parameters = @{
     "Name" = "bbbb-is-the-word"
     "ResourceGroupName" = $rgName
-    "TemplateUri"="https://raw.githubusercontent.com/4c74356b41/bbbb-is-the-word/master/_arm/parent.json"
+    "TemplateUri"="https://raw.githubusercontent.com/4c74356b41/bbbb-is-the-word/$feature/_arm/parent.json"
     "TRAFFICMANAGERNAME"="$rgName-TM"
     "SKU"="S1"
+    "BRANCH"="$feature"
     "ADMINISTRATORLOGIN"="dba"
-    "VAULTNAME"=$vaultName
     "SECRETNAME"="administratorLoginPassword"
     "DATABASENAME"="database"
     "ErrorAction"="stop"
@@ -35,4 +35,4 @@ $functionWebAppName = $deploy.outputs.values.value[2]
 $sqlServerPrimary = $deploy.Outputs.Values.value[3]
 
 $tests = Invoke-Pester -OutputFormat NUnitXml -OutputFile tests.xml -PassThru
-if ($tests.FailedCount -gt 12) { Remove-AzureRmResourceGroup -ResourceGroupName $rgName -Force }
+if ($tests.FailedCount -eq 0) { Remove-AzureRmResourceGroup -ResourceGroupName $rgName -Force }
